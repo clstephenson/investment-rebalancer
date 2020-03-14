18dafbfa26e07f134e32a8726ee18e83a02a34f5
Chris Stephenson <clstephenson@gmail.com>
2020-03-13 17:35:48 -0700

    Rename class.

--- src/test/java/com/clstephenson/investmentrebalancer/commandrunner/commands/UpdateAssetTest.java
+++ src/test/java/com/clstephenson/investmentrebalancer/commandrunner/commands/UpdateAssetTest.java
@@ -20,7 +20,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
-class AddHoldingTest {
+class UpdateAssetTest {
 
     @Mock
     Context context;
